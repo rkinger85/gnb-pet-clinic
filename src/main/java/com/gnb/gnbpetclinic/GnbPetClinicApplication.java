2cefe4aae1de75744d3d02504ae30edded7fc565
rishi <rkinger85@gmail.com>
2020-03-11 16:21:54 +0800

    Initial Project Add

---
+++ src/main/java/com/gnb/gnbpetclinic/GnbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.gnb.gnbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GnbPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gnb/gnbpetclinic/GnbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnb.gnbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnbPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
