3ac8b5fbc416dc768a774fd019c92729fb8a90cc
rishi <rkinger85@gmail.com>
2020-03-24 16:31:11 +0800

    Impl of Vet and Owner controllers and Index pages. closes #5

---
+++ pet-clinic-web/src/main/java/com/gnb/gnbpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.gnb.gnbpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"","/index","/index.html"})
+    public String listOwners(){
+
+        return "owners/index";
+    }
+}
